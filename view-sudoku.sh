8cf93dac906522f93cf925ec2167556b7e8ead42
Hyejeong Im <hyejeongim@macbook-pro-de-hyejeong.home>
2022-10-26 21:05:59 +0200

    RENDU sauf la question 3 et ajout de quelques exemples pour tester dpll

---
+++ view-sudoku.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# à utiliser sur un fichier modèle retourné par glucose ou minisat
+printf "+---+---+---+\n|"
+
+i=1 # numéro d'entrée courante
+for n in `grep -v 'SAT' $1 | sed -e 's/-[0-9]* //g;s/ 0//g;s/[1-9][1-9]\([1-9]\)/\1/g' | sort`
+do
+    printf "$n"
+    if !(( i % 3 ))
+    then
+        printf "|"
+    fi
+    if !(( i % 9 ))
+    then
+        printf "\n"
+    fi
+    if !((i % 27))
+    then
+        echo "+---+---+---+"
+    fi
+    if !(( i % 9 )) && (( i < 81 ))
+    then
+        printf "|"
+    fi
+    i=$((i+1))
+done
